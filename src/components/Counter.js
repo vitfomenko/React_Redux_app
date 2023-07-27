339cd85ec8cd606f8989befbf738d0127aa7b135
Vitalii Fomenko <vfomenko@Vitaliis-MacBook-Pro-23310.local>
2023-07-27 13:56:16 +0300

    small app based on react and redux

---
+++ src/components/Counter.js
@@ -0,0 +1,28 @@
+// import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { inc, dec, rnd } from '../actions';
+
+const Counter = () => {
+
+    const counter = useSelector(state => state.counter);
+    const dispatch = useDispatch();
+
+    return (
+        <div className="jumbotron">
+            <h1>{counter}</h1>
+            <button onClick={() => dispatch(dec())} className="btn btn-primary">DEC</button>
+            <button onClick={() => dispatch(rnd())} className="btn btn-primary">RND</button>
+            <button onClick={() => dispatch(inc())} className="btn btn-primary">INC</button>
+        </div>
+    );
+}
+
+// const mapStateToProps = (state) => {
+//     return {
+//         counter: state.value
+//     }
+// };
+
+// export default connect(mapStateToProps, actions)(Counter);
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createRoot} from 'react-dom/client';
+import {createStore} from 'redux';
+import reducer from './reducer';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './components/App';
+
+const store = createStore(reducer,
+              window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
